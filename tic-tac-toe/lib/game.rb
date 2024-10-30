0b5287571982b96a336a6cc9e7871e2eebfbdeea
KamogeloMahlake <3949843@myuwc.ac.za>
2024-10-30 10:31:17 +0200

    change main file while loop

--- tic-tac-toe/lib/game.rb
+++ tic-tac-toe/lib/game.rb
@@ -1,52 +1,52 @@
+# frozen_string_literal: true
+
 class Game
   attr_accessor :squares, :current_winner
+
   def initialize
-    @squares = [1, 2, 3,4,5,6,7,8,9]
+    @squares = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     @current_winner = false
   end
-  
+
   def to_s
     x = 0
     board = "---+---+---\n"
-    for i in 0..2
-      row = ""
-      for j in 0..2
+    (0..2).each do |_|
+      row = ''
+      (0..2).each do |j|
         row += j == 2 ? " #{@squares[x]}" : " #{@squares[x]} |"
         x += 1
       end
-      board += row + "\n" + "---+---+---" +"\n"
+      board += "#{row}\n---+---+---\n"
     end
-    return board
+    board
   end
-  
+
   def make_move(square, letter)
     if @squares[square - 1].is_a?(Integer)
-      self.squares[square - 1] = letter
+      squares[square - 1] = letter
 
-      if self.winner(letter)
-        self.current_winner = letter
-      end
+      self.current_winner = letter if winner(letter)
       return true
-    end  
-    return false
+    end
+    false
   end
 
   def winner(letter)
     winning_lines = [
-    @squares.first(3).all? {|square| square == letter},
-    @squares.last(3).all? {|square| square == letter},
-    @squares[3..5].all? {|square| square == letter},
-    @squares.values_at(0, 3, 6).all? {|square| square == letter},
-    @squares.values_at(1, 4, 7).all? {|square| square == letter},
-    @squares.values_at(2, 5, 8).all? {|square| square == letter},
-    @squares.values_at(0, 4, 8).all? {|square| square == letter},
-    @squares.values_at(2, 4, 6).all? {|square| square == letter}
+      @squares.first(3).all? { |square| square == letter },
+      @squares.last(3).all? { |square| square == letter },
+      @squares[3..5].all? { |square| square == letter },
+      @squares.values_at(0, 3, 6).all? { |square| square == letter },
+      @squares.values_at(1, 4, 7).all? { |square| square == letter },
+      @squares.values_at(2, 5, 8).all? { |square| square == letter },
+      @squares.values_at(0, 4, 8).all? { |square| square == letter },
+      @squares.values_at(2, 4, 6).all? { |square| square == letter }
     ]
-    return winning_lines.any? {|i| i == true} 
+    winning_lines.any? { |i| i == true }
   end
 
-  def get_available_moves
-    return self.squares.select{|i| i if i.is_a?(Integer)}
+  def available_moves
+    squares.select { |i| i if i.is_a?(Integer) }
   end
 end
-
--- tic-tac-toe/main.rb
+++ tic-tac-toe/main.rb
@@ -1,68 +1,43 @@
-require_relative "lib/game"
-require_relative "lib/player"
+# frozen_string_literal: true
 
+require_relative 'lib/game'
+require_relative 'lib/player'
 
-
-def get_human_player_letter
-  i = 0
-  letters = ["X", "O"]
-  while i < 1 do
-    print "X or O: "
+def human_player_letter
+  letters = %w[X O]
+  loop do
+    print 'X or O: '
     letter = gets.chomp.upcase
 
-    if letters.include?(letter)
-      i += 1
-    end
+    return letter if letters.include?(letter)
   end
-  
-  return letter
 end
-human_letter = get_human_player_letter
-computer_letter = human_letter == "X" ? "O" : "X"
+human_letter = human_player_letter
+computer_letter = human_letter == 'X' ? 'O' : 'X'
 
 def play_game(human_letter, computer_letter)
   game = Game.new
   human = HumanPlayer.new(human_letter)
   computer = ComputerPlayer.new(computer_letter)
-
   puts game
-  while (game.current_winner == false) do
-    begin
-      if human_letter == "X"
-        player_move = human.get_move(game.get_available_moves)
-        game.make_move(player_move, human_letter)
-        puts "#{human_letter} makes a move to square #{player_move}"
-      
-        sleep 0.8
-
-        computer_move = computer.get_move(game.get_available_moves)
-        game.make_move(computer_move, computer_letter)
-        puts "#{computer_letter} makes a move to square #{computer_move}"
-
-      else
-        computer_move = computer.get_move(game.get_available_moves)
-        game.make_move(computer_move, computer_letter)
-        puts "#{computer_letter} makes a move to square #{computer_move}"
-        puts game
-        sleep 0.8
-        player_move = human.get_move(game.get_available_moves)
-        game.make_move(player_move, human_letter)
-        puts "#{human_letter} makes a move to square #{player_move}"
-      end
-      
-      puts game
-    rescue StandardError
-      break
+  letter = 'X'
+  while game.available_moves
+    if human_letter == letter
+      player_move = human.get_move(game.available_moves)
+      game.make_move(player_move, human_letter)
+      puts "#{letter} make move to square #{player_move}"
+    else
+      computer_move = computer.get_move(game.available_moves)
+      game.make_move(computer_move, computer_letter)
+      puts "#{letter} make move to square #{computer_move}"
     end
+    puts game
+    return " #{game.current_winner} is the winner" if game.winner(letter)
+
+    letter = letter == 'X' ? 'O' : 'X'
+    sleep 0.8
   end
-  if game.current_winner == false
-    return "no available moves\nIt's a tie!"
-  end
-  return " #{game.current_winner} is the winner"
+  "no available moves\nIt's a tie!"
 end
 
-puts play_game(human_letter,computer_letter)
-
-
-
-
+puts play_game(human_letter, computer_letter)
