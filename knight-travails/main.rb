f39ed5c79bb84a797ec7f70f36be2ef8ab161bfc
KamogeloMahlake <3949843@myuwc.ac.za>
2024-12-18 12:42:39 +0200

    rubocop

--- knight-travails/main.rb
+++ knight-travails/main.rb
@@ -1,5 +1,8 @@
-class Node 
+# frozen_string_literal: true
+
+class Node
   attr_accessor :value, :previous
+
   def initialize(value)
     @previous = nil
     @value = value
@@ -9,19 +12,18 @@ end
 def all_possible_moves(edge)
   moves = [[2, 1], [2, -1], [-2, 1], [-2, -1], [1, 2], [-1, 2], [1, -2], [-1, -2]]
 
-  moves.map {|x, y| [x + edge[0], y + edge[1]]}.filter{|i, j| (0..7).include?(i) && (0..7).cover?(j)}
+  moves.map { |x, y| [x + edge[0], y + edge[1]] }.filter { |i, j| (0..7).include?(i) && (0..7).include?(j) }
 end
 
 def final_node(start, finish)
   queue = [Node.new(start)]
-  loop do 
+  loop do
     return queue[0] if queue[0].value == finish
 
     all_possible_moves(queue[0].value).each do |move|
       move = Node.new(move)
       move.previous = queue[0]
       queue << move
-
     end
     queue.delete_at 0
   end
@@ -34,11 +36,11 @@ def knight_moves(start, finish)
     array.unshift(node.value)
     node = node.previous
   end
-  
-  str = "You make it in #{array.length -  1} moves! Here's your path:\n"
-  
-  array.each {|move| str += "#{move}\n"}
+
+  str = "You make it in #{array.length - 1} moves! Here's your path:\n"
+
+  array.each { |move| str += "#{move}\n" }
   str
 end
 
-puts knight_moves([4,3], [3,3])
+puts knight_moves([4, 3], [3, 3])
